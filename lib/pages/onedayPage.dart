87e65d557ca19e350710729e7ae6cd636895f06d
mouloud240 <m.hasrane@esi-sba.dz>
2024-07-02 17:33:22 +0100

    Finished the homePage

--- lib/pages/onedayPage.dart
+++ lib/pages/onedayPage.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class Onedaypage extends StatelessWidget {
   const Onedaypage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Text("One day page"),
+    );
   }
 }
