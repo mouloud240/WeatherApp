0200403a2518b8bc83c2a8c2690073d4cdd3778c
mouloud240 <m.hasrane@esi-sba.dz>
2024-07-02 18:28:17 +0100

    fixed the bg

--- lib/pages/5dayForecast.dart
+++ lib/pages/5dayForecast.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class daysForecast extends StatelessWidget {
   const daysForecast({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Text('Five day page');
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage("assets/bg.png"), fit: BoxFit.fill)),
+      ),
+    )
   }
 }
--- lib/pages/onedayPage.dart
+++ lib/pages/onedayPage.dart
@@ -1,13 +1,28 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class Onedaypage extends StatelessWidget {
+class Onedaypage extends StatefulWidget {
   const Onedaypage({super.key});
 
   @override
+  State<Onedaypage> createState() => _OnedaypageState();
+}
+
+class _OnedaypageState extends State<Onedaypage> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text("One day page"),
-    );
+        body: SizedBox.expand(
+          child: Container(
+            decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage("assets/bg.png"), fit: BoxFit.fill)),
+          ),
+        ),
+        bottomNavigationBar: BottomAppBar(
+          child: Row(
+            children: [Icon(Icons.menu)],
+          ),
+        ));
   }
 }
